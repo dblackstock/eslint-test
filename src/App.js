b8798aab8a4d3273bf9809deed760a5c90bd26bf
dblackstock <51056503+dblackstock@users.noreply.github.com>
2020-02-03 12:14:55 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+  import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+
+  const testFunction = () => {
+    let anObject = {key:"words"}          
+    return anObject.key;
+  }
+
+   return (
+    <div className='App'>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. {testFunction}
         </p>
         <a
           className="App-link"
