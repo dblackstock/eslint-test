519d5579b0d801a93f1f14d8c23961768370694c
dblackstock <51056503+dblackstock@users.noreply.github.com>
2020-02-05 10:28:00 +0000

    test

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. {testFunction}
-          {anotherTest}
         </p>
         <a
           className="App-link"
