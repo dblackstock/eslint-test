56affd803f340e014b7d14e13306140f38242288
dblackstock <51056503+dblackstock@users.noreply.github.com>
2020-02-05 09:21:06 +0000

    this should pass with the new pre-commit script

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 
 function App() {
   const anotherTest = () => {
-    let someThing = "words and again";
+    let someThing = "words";
     console.log(someThing);
   };
 
@@ -19,6 +19,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. {testFunction}
+          {anotherTest}
         </p>
         <a
           className="App-link"
